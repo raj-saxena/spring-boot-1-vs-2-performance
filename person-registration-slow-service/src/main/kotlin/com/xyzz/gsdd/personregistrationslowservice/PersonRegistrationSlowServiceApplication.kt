9f6364781731332da6326a8d0f936182833aa4f1
Raj <the.raj.saxena@gmail.com>
2018-03-01 17:04:37 +0100

    Added person-registration-slow-service

---
+++ person-registration-slow-service/src/main/kotlin/com/xyzz/gsdd/personregistrationslowservice/PersonRegistrationSlowServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.xyzz.gsdd.personregistrationslowservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PersonRegistrationSlowServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<PersonRegistrationSlowServiceApplication>(*args)
+}
