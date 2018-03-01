49206074627ded22fdc224a0aae491c20d4b2fe7
Raj <the.raj.saxena@gmail.com>
2018-03-01 14:15:39 +0100

    Added Gatling load test

---
+++ perf-test-with-gatling/src/test/scala/BootLoadSimulation.scala
@@ -0,0 +1,34 @@
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+
+class BootLoadSimulation extends Simulation {
+
+  private val baseUrl = "http://localhost:8080"
+  private val endpoint = "/persons"
+  private val contentType = "application/json"
+  private val requestCount = 5
+
+  private val simUsers = System.getProperty("SIM_USERS", "1").toInt
+
+  private val httpConf = http
+    .baseURL(baseUrl)
+    .acceptHeader("application/json;charset=UTF-8")
+
+  private val addPersonTest = repeat(requestCount) {
+    exec(http("add-person-test")
+      .post(endpoint)
+      .header("Content-Type", contentType)
+      .body(StringBody(
+        s"""
+           | {
+           |  "firstName": "test-firstName",
+           |  "lastName": "test-lastName"
+           | }
+         """.stripMargin
+      )).check(status.is(201)))
+  }
+  private val scn = scenario("BootLoadSimulation")
+    .exec(addPersonTest)
+
+  setUp(scn.inject(atOnceUsers(simUsers))).protocols(httpConf)
+}
